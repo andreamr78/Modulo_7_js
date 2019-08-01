84906ba4c355818d0a5fa9c98e7ce62f3ee39963
andreamr78 <52381170+andreamr78@users.noreply.github.com>
2019-08-01 18:51:17 -0500

    Add files via upload
    tarea y trabajo en clase

---
+++ practica3/app.js
@@ -0,0 +1,18 @@
+function imagen1() {
+    var source = "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313__340.jpg" 
+    var img = document.getElementById('img')
+    img.src = source.replace();
+    img.style.display = "block";
+}
+function imagen2() {
+    var source = "https://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg" 
+    var img = document.getElementById('img')
+    img.src = source.replace();
+    img.style.display = "block";
+}
+function imagen3() {
+    var source = "https://c8.alamy.com/comp/C84GTH/funny-puppy-in-sunglasses-wearing-a-necktie-C84GTH.jpg" 
+    var img = document.getElementById('img')
+    img.src = source.replace();
+    img.style.display = "block";
+}
